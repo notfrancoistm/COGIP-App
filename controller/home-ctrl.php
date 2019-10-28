41b602d7a5a44c9b44967d8b66d4911fd14b136b
FrancoisTM <francoisthibdemais@outlook.com>
2019-10-28 13:05:18 +0100

    adding home model & controller

---
+++ controller/home-ctrl.php
@@ -0,0 +1,7 @@
+<?php 
+require 'view/component/header-component.php';
+require 'view/component/menu-component.php';
+require 'view/home-view.php';
+require 'model/home-mdl.php';
+require 'view/component/footer-component.php';
+?>
