2635a15e75a4069084fe06b33c6cf1b524c4c268
wailmadrane <wail.madrane@gmail.com>
2019-10-28 15:47:49 +0100

    routing

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-$d = dir(".");
-echo "<table style='border:1px solid black;'><td>";
-while ($entry = $d->read()) {
-    ?> <a href="<?php echo $entry; ?>"><?php echo $entry; ?> </a><br><?php
-}
-$d->close();
-echo "</td></table>";
+    // routeing
+	if (!empty($_GET['page'])) {
+        $page = trim(addslashes($_GET['page']));
+		$path = __DIR__ . DIRECTORY_SEPARATOR . 'controllers'. DIRECTORY_SEPARATOR . $page . '-ctrl.php';
+		if (is_file($path)) {
+			require $path;
+		}
+	}
+	else {
+        $page = 'home';
+		require __DIR__ . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'home-ctrl.php';
+    }
 ?>
--- view/home-view.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'view/component/header-component.php';
-require 'view/component/menu-component.php';
-?>
-
-
-<?php 
-require 'view/component/footer-component.php';
-?>
