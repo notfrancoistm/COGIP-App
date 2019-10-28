05f9cb2252541663761736e4f5e03250b2fcdac1
wailmadrane <wail.madrane@gmail.com>
2019-10-28 15:49:53 +0100

    routing

---
+++ controllers/home-ctrl.php
@@ -0,0 +1,4 @@
+<?php
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $page . '-view.php';
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . $page . '-mdl.php';
+?>
---
+++ views/home-view.php
@@ -0,0 +1,9 @@
+<?php
+require 'views/component/header-component.php';
+require 'views/component/menu-component.php';
+?>
+
+
+<?php 
+require 'views/component/footer-component.php';
+?>
