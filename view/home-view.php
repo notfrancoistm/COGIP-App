19bca1774f5496c551db5bb5008b60a759b9affd
FrancoisTM <francoisthibdemais@outlook.com>
2019-10-29 09:58:38 +0100

    button settings

---
+++ view/home-view.php
@@ -0,0 +1,14 @@
+<?php 
+require 'component/header-component.php'; //temporary
+?>
+
+<div class="btn-container">
+    <div class="btn-container-box">
+        <button type="button" class="btn btn-outline-success btn-lg">Last invoice</button>
+        <button type="button" class="btn btn-outline-success btn-lg">Last contacts</button>
+        <button type="button" class="btn btn-outline-success btn-lg">Last compagnies</button>
+    </div>
+</div>
+
+<div class="info-container">
+</div>
