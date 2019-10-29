7f18f8dcf576b120ca321114c365921bba5d90b7
FrancoisTM <francoisthibdemais@outlook.com>
2019-10-29 10:47:57 +0100

    setting home view bascis

--- view/home-view.php
+++ view/home-view.php
@@ -4,11 +4,14 @@ require 'component/header-component.php'; //temporary
 
 <div class="btn-container">
     <div class="btn-container-box">
-        <button type="button" class="btn btn-outline-success btn-lg">Last invoice</button>
-        <button type="button" class="btn btn-outline-success btn-lg">Last contacts</button>
-        <button type="button" class="btn btn-outline-success btn-lg">Last compagnies</button>
+        <button type="button" class="btn btn-outline-success ">Last invoice</button>
+        <button type="button" class="btn btn-outline-success ">Last contacts</button>
+        <button type="button" class="btn btn-outline-success ">Last compagnies</button>
     </div>
 </div>
 
 <div class="info-container">
+    <div class="btn-container-box">
+        ...
+    </div>
 </div>
