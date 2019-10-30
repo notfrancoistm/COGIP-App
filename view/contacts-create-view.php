876965d934ee216e2ba34a69d51c95cd12f89179
FrancoisTM <francoisthibdemais@outlook.com>
2019-10-30 09:42:55 +0100

    adding create form

---
+++ view/contacts-create-view.php
@@ -0,0 +1,15 @@
+<form>
+    <h3>New contact</h3>
+    <div class="row">
+        <div class="col">
+            <input type="text" class="form-control" placeholder="First name">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Last name">
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control" id="formGroupExampleInput" placeholder="email adress">
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Create contact</button>
+</form>
