e4d706474366e04f637b33ec8f93ba99b2738a6a
FrancoisTM <francoisthibdemais@outlook.com>
2019-10-30 15:25:09 +0100

    update

--- view/contacts-create-view.php
+++ view/contacts-create-view.php
@@ -1,15 +1,37 @@
-<form>
-    <h3>New contact</h3>
-    <div class="row">
-        <div class="col">
-            <input type="text" class="form-control" placeholder="First name">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control" placeholder="Last name">
-        </div>
-        <div class="form-group">
-            <input type="email" class="form-control" id="formGroupExampleInput" placeholder="email adress">
-        </div>
+<div class="row justify-content-center">
+<div class="card text-center  col-md-8">
+    <h5 class="row card-header info-color py-3">
+        <strong>New contact</strong>
+    </h5>
+        <!-- Form -->
+        <form class="text-center" style="color: #757575;" action="#!">
+            <!-- first name row -->
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <label for="">First name</label>
+                    <input type="text" class="form-control" placeholder="First name">
+                </div>
+            </div>
+
+            <!-- last name row -->
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <label for="">Last name</label>
+                    <input type="text" class="form-control" placeholder="Last name">
+                </div>
+            </div>
+
+            <!-- email name row -->
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <label for="">Email adress</label>
+                    <input type="email" class="form-control" placeholder="Email adress">
+                </div>
+            </div>
+           
+            <!-- Create button -->
+            <button type="submit" class="btn btn-primary btn-rounded waves-effect">Create contact</button>
+        </form>
     </div>
-    <button type="submit" class="btn btn-primary">Create contact</button>
-</form>
+</div>
+</div>
