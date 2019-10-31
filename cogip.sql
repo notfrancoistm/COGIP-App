dfbab21fd0e4cd7381c337a3444fc1def5feff07
Alice R <alice.rouben@gmail.com>
2019-10-31 10:36:18 +0100

    update cogip : remplissage

--- cogip.sql
+++ cogip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: database:3306
--- Generation Time: Oct 31, 2019 at 08:27 AM
+-- Generation Time: Oct 31, 2019 at 09:33 AM
 -- Server version: 10.4.2-MariaDB-1:10.4.2+maria~bionic
 -- PHP Version: 7.2.5
 
@@ -41,7 +41,14 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`ID`, `company_name`, `VAT`, `country`, `type`) VALUES
-(1, 'Dunder Mifflin', 'US678765765', 'USA', 1);
+(1, 'dunder mifflin', 'us678765765', 'usa', 1),
+(2, 'raviga', 'US456654342', 'usa', 1),
+(3, 'jouet jean-michel', 'fr677544000', 'france', 1),
+(4, 'bob vance refrig.', 'us456654687', 'usa', 1),
+(5, 'belgalol', 'be0876654665', 'belgium', 2),
+(6, 'pierre cailloux', 'fr678908654', 'france', 2),
+(7, 'proximdr', 'be0876985665', 'belgique', 2),
+(8, 'electric power', 'it256852542', 'italy', 2);
 
 -- --------------------------------------------------------
 
@@ -63,7 +70,14 @@ CREATE TABLE `contacts` (
 --
 
 INSERT INTO `contacts` (`ID`, `first_name`, `last_name`, `mail`, `company`, `phone`) VALUES
-(6, 'Dwight', 'Schrute', 'dwight.schrute@ddmfl.com', 1, 555511984);
+(1, 'Dwight', 'Schrute', 'dwight.schrute@ddmfl.com', 1, 555511984),
+(15, 'peter', 'gregory', 'peter.gregory@raviga.com', 2, 5554567),
+(16, 'cameron', 'howe', 'cam.howe@belgalol.net', 5, 5557896),
+(17, 'gavin', 'belson', 'gavin@cailloux.fr', 6, 5554213),
+(18, 'jian', 'yang', 'jian.yang@vance.bob', 4, 5554567),
+(19, 'bertram', 'gilfoye', 'gilfoye.b@electricpow.it', 8, 5550987),
+(20, 'pascale', 'belette', 'pas.bel@jjmc.fr', 3, 5489632),
+(21, 'archie', 'medesi', 'archi.mede@proximdr.net', 7, 6335841);
 
 -- --------------------------------------------------------
 
@@ -85,10 +99,17 @@ CREATE TABLE `invoices` (
 --
 
 INSERT INTO `invoices` (`ID`, `number`, `date`, `company`, `company_type`, `contact`) VALUES
-(3, 'F20190404-004', '2019-10-30 00:00:00', 1, 1, 6),
-(4, 'F20180310-052', '2019-10-30 00:00:00', 1, 1, 6),
-(5, 'F20190204-008', '2019-10-30 00:00:00', 1, 1, 6),
-(6, 'F20160409-013', '2019-10-30 00:00:00', 1, 1, 6);
+(1, 'F20190404-004', '2019-10-30 00:00:00', 1, 1, 1),
+(2, 'F20180310-052', '2019-10-30 00:00:00', 1, 1, 1),
+(3, 'F20190204-008', '2019-10-30 00:00:00', 1, 1, 1),
+(4, 'F20160409-013', '2019-10-30 00:00:00', 1, 1, 1),
+(7, 'f20190404-004', '2019-04-04 00:00:00', 3, 2, 20),
+(8, 'F20190404-002', '2019-04-04 00:00:00', 6, 2, 17),
+(9, 'F20190404-001', '2019-04-04 00:00:00', 5, 2, 16),
+(10, 'F20190403-654', '2019-04-03 00:00:00', 2, 1, 15),
+(11, 'f20150522-235', '2015-05-22 00:00:00', 4, 1, 18),
+(12, 'f20101103-369', '2010-11-03 00:00:00', 8, 2, 19),
+(13, 'f20160226-245', '2016-02-26 00:00:00', 7, 2, 21);
 
 -- --------------------------------------------------------
 
@@ -177,19 +198,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `type`
