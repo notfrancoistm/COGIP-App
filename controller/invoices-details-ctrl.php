2d2055ca09a08cb108ce721b256da9d881a037b6
wailmadrane <wail.madrane@gmail.com>
2019-11-05 09:12:44 +0100

    adding details

--- controller/invoices-details-ctrl.php
+++ controller/invoices-details-ctrl.php
@@ -1,9 +1,6 @@
 <?php
     require 'model/invoices-details-mdl.php';
     $id = $_GET['id'];
-    $invoices_data = get_by_id('invoices', $id);
-    echo '<pre>';
-        var_dump($invoices_data);
-    echo '</pre>';
+    $invoice = get_by_id('invoices', $id);
     require 'view/invoices-details-view.php';
 ?>
--- view/invoices-details-view.php
+++ view/invoices-details-view.php
@@ -5,33 +5,23 @@
         <a href="?page=invoices-edit"><button type="button" class="btn btn-outline-secondary">edit invoices</button></a>
         <a href="?page=invoices-delete"><button type="button" class="btn btn-outline-danger">delete invoices</button></a>
     </div>
-
-    <table class="table table-striped table-responsive-sm">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Invoice Number</th>
-                <th scope="col">Date</th>
-                <th scope="col">Company</th>
-                <th scope="col">Delete</th>
-                <th scope="col">Update</th>
-                <th scope="col">Details</th>          
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($invoices_data as $invoice) :?>
-            <tr>          
-                <th scope="row"><?=$invoice['ID']?></th>
-                <td><?=$invoice['number']?></td>
-                <td><?=date('Y-m-d', strtotime($invoice['date']))?></td>
-                <td><?=$invoice['company']?></td>
-                <?php if ($_SESSION['rights'] === 'god') :?>
-                <td><a href="?page=invoices-delete&id=<?=$invoice['ID']?>" class="fa fa-trash" aria-hidden="true"></a></td>
-                <td><a href="?page=invoices-edit&id=<?=$invoice['ID']?>" class="fa fa-pencil" aria-hidden="true"></a></td>
-                <td><a href="?page=invoices-details&id=<?=$invoice['ID']?>" class="fa fa-eye" aria-hidden="true"></a></td>          
-                <?php endif ?>
-            </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
 </div>
+
+<table class="table table-striped table-responsive-sm">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Invoice Number</th>
+            <th scope="col">Date</th>
+            <th scope="col">Company</th>          
+        </tr>
+    </thead>
+    <tbody>
+        <tr>          
+            <th scope="row"><?=$invoice['ID']?></th>
+            <td><?=$invoice['number']?></td>
+            <td><?=date('Y-m-d', strtotime($invoice['date']))?></td>
+            <td><?=$invoice['company']?></td>
+        </tr>
+    </tbody>
+</table>
