178620a64768e1726bdf08c3b19f8880553d8efb
wailmadrane <wail.madrane@gmail.com>
2019-11-08 15:52:21 +0100

    trying to fix gender bug

--- view/contacts-edit-view.php
+++ view/contacts-edit-view.php
@@ -1,5 +1,5 @@
 <?php
-// dump($contact);
+dump($contact);
 // dump($companies_data[0]);
 ?>
 <div class="globalelement row justify-content-center">
@@ -13,25 +13,29 @@
 
                 <div class="form-group col-md-8">
                     <label for="first_name">first name</label>
-                    <input class="form-control" name="first_name" value="<?=$contact['first_name']?>" id="first_name" type="text" placeholder="First name" required autofocus>
+                    <input class="form-control" name="first_name" value="<?=$first_name?>" id="first_name" type="text" placeholder="First name" required autofocus>
                 </div>
 
                 <div class="form-group col-md-8">
                     <label for="last_name">last name</label>
-                    <input class="form-control" name="last_name" value="<?=$contact['last_name']?>" id="last_name" type="text" placeholder="Last name" required>
+                    <input class="form-control" name="last_name" value="<?=$last_name?>" id="last_name" type="text" placeholder="Last name" required>
                 </div>
 
                 <div class="form-group col-md-8">
                     <label for="email">mail</label>
-                    <input class="form-control" name="email" value="<?=$contact['mail']?>" id="email" type="text" placeholder="Email adress" required>
+                    <input class="form-control" name="email" value="<?=$email?>" id="email" type="text" placeholder="Email adress" required>
+                </div>
+
+                <div class="form-group col-md-8">
+                    <label for="phone">phone</label>
+                    <input class="form-control" name="phone" value="<?=$phone?>" id="phone" type="tel" placeholder="phone" required>
                 </div>
 
                 <div class="form-group col-md-8">
                     <label for="company">company</label>
                     <select class="form-control" name="company" id="company" required>
-                    <option value="none" selected disabled hidden>Company</option> 
                     <?php foreach($companies_data as $company) : ?>
-                        <option value="<?=$company['company_id']?>" <?=is_selected($contact['company'], $company['company_id'])?> ><?=$company['company_name']?></option>
+                        <option value="<?=$company['company_id']?>" <?=is_selected($contact_company, $company['company_id'])?> ><?=$company['company_name']?></option>
                     <?php endforeach ?>
                     </select>
                 </div>
