f6d0f8724f024936f39f62f07d87b80343f31c1d
wailmadrane <wail.madrane@gmail.com>
2019-11-12 22:36:49 +0100

    create companies

--- controller/compagnies-create-ctrl.php
+++ controller/compagnies-create-ctrl.php
@@ -1,5 +1,26 @@
 <?php
 require 'model/compagnies-create-mdl.php';
+
+// get previous data to display on the inputs values
 $types = get_all_types();
+
+// assign user value form the form else use the previous data from the db
+$company_name = $_POST['company_name'] ?? '';
+$vat_number = $_POST['vat_number'] ?? '';
+$country = $_POST['country'] ?? '';
+$company_type = $_POST['type'] ?? '';
+
+// validate inputs
+$company_name__val = string_validation($company_name);
+$vat_number__val = vat_validation($vat_number);
+$country__val = country_validation($country);
+$company_type__val = id_validation($company_type);
+$submit = isset($_POST['submit']);
+
+// if all is valide update db
+if ($company_name__val AND $vat_number__val AND $country__val AND $company_type__val AND $submit) {
+   create_company($company_name, $vat_number, $country, $company_type);
+}
+
 require 'view/compagnies-create-view.php';
 ?>
--- view/compagnies-create-view.php
+++ view/compagnies-create-view.php
@@ -8,34 +8,36 @@
             <div class="row justify-content-center">
 
                 <div class="form-group col-md-8">
-                    <input class="form-control" name="company_name" id="company_name" type="text" placeholder="Name of the company" required autofocus>
+                    <label for="company_name">company</label>
+                    <input class="form-control" name="company_name" value="<?=$company_name?>" id="company_name" type="text" placeholder="Name of the company" required autofocus>
                 </div>
 
                 <div class="form-group col-md-8">
-                    <input class="form-control" name="vat_number" id="vat_number" type="text" placeholder="VAT of the company" required>
+                    <label for="vat_number">VAT</label>
+                    <input class="form-control" name="vat_number" value="<?=$vat_number?>" id="vat_number" type="text" placeholder="VAT of the company" required>
                 </div>
 
                 <div class="form-group col-md-8">
-
+                    <label for="country">country</label>
                     <select class="form-control" name="country" id="country" required>                  
-                        <?php require 'component/country.php' ?>
+                    <?php require 'component/country.php' ?>
 
-                        <?php foreach($countries as $key => $value) : ?> 
-                            <option value="<?= $key ?>" <?=is_selected($country, $key)?> title="<?= htmlspecialchars($value) ?>"><?= htmlspecialchars($value) ?></option> 
-                        <?php endforeach ?>
+                    <?php foreach($countries as $key => $value) : ?> 
+                        <option value="<?= $key ?>" <?=is_selected($country, $key)?> title="<?= htmlspecialchars($value) ?>"><?= htmlspecialchars($value) ?></option> 
+                    <?php endforeach ?>
                     
                     </select>
                 </div>
 
                 <div class="form-group col-md-8">
-
+                    <label for="type">company type</label>
                     <select class="form-control" name="type" id="type" required>
-                        <?php foreach($types as $type) :?>     
-                            <option value="<?=$type['ID']?>" <?=is_selected($company_type, $type['ID'])?> ><?=$type['company_type']?></option>
-                        <?php endforeach ?>
+                    <?php foreach($types as $type) :?>     
+                        <option value="<?=$type['ID']?>" <?=is_selected($company_type, $type['ID'])?> ><?=$type['company_type']?></option>
+                    <?php endforeach ?>
                     </select>
                 </div>
-            </div>  
+            </div>   
                 
             <div class="row justify-content-center">
                 <button type="submit" name="submit" class="btn btn-rounded" style="background-color: rgb(112, 193, 247);" value="Login">Create company</button>
